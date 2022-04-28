bfc6e6ceae49305abe74f62ca33d2fa5ac70a98d
mark <markel3266@gmail.com>
2022-04-28 20:13:44 +0300

    add support of folders in NewValidator

--- csv.go
+++ csv.go
@@ -5,6 +5,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"path"
 )
 
 var BOM = []byte{239, 187, 191}
@@ -12,7 +13,7 @@ var BOM = []byte{239, 187, 191}
 func removeBOM(records [][]string) {
 	if len(records) != 0 {
 		firstRecord := []byte(records[0][0])
-		if bytes.Compare(BOM, firstRecord[:3]) == 0 {
+		if bytes.Equal(BOM, firstRecord[:3]) {
 			records[0][0] = string(firstRecord[3:])
 		}
 	}
@@ -30,4 +31,21 @@ func readCSV(filePath string) ([][]string, error) {
 	}
 	removeBOM(records)
 	return records, nil
+}
+
+func getHeaders(records [][]string) map[string]struct{} {
+	headers := make(map[string]struct{})
+	for i, row := range records {
+		if i != 0 {
+			break
+		}
+		for _, h := range row {
+			headers[h] = struct{}{}
+		}
+	}
+	return headers
+}
+
+func isCSV(filePath string) bool {
+	return path.Ext(filePath) == ".csv"
 }
--- csv_test.go
+++ csv_test.go
@@ -1,6 +1,7 @@
 package csvalidator
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -34,5 +35,50 @@ func TestReadCSV(t *testing.T) {
 		records, err := readCSV(tc.filePath)
 		assert.NoError(t, err)
 		assert.Equal(t, tc.expected, records)
+	}
+}
+
+func TestGetHeaders(t *testing.T) {
+	r, err := readCSV("./samples/file.csv")
+	assert.NoError(t, err)
+	headers := getHeaders(r)
+
+	expected := map[string]struct{}{
+		"id":      {},
+		"comment": {},
+	}
+
+	headersJson, err := json.Marshal(headers)
+	assert.NoError(t, err)
+	expectedJson, err := json.Marshal(expected)
+	assert.NoError(t, err)
+	assert.JSONEq(t, string(expectedJson), string(headersJson))
+}
+
+func TestIsCSV(t *testing.T) {
+	cases := []struct {
+		path     string
+		expected bool
+	}{
+		{
+			path:     "./folder/file.csv",
+			expected: true,
+		},
+		{
+			path:     "file.csv",
+			expected: true,
+		},
+		{
+			path:     "./folder/file.txt",
+			expected: false,
+		},
+		{
+			path:     "e.txt",
+			expected: false,
+		},
+	}
+
+	for _, tc := range cases {
+		assert.Equal(t, tc.expected, isCSV(tc.path))
 	}
 }

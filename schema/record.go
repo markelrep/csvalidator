4c1478d5cd4a7dae549eb9c0b917057c1b5e4490
mark <markel3266@gmail.com>
2022-07-21 19:46:52 +0300

    fix no op

--- schema/record.go
+++ schema/record.go
@@ -5,33 +5,33 @@ import (
 	"regexp"
 )
 
-// contains is field in schema which uses to define validation type and validation rule for data in cell
-type contains struct {
+// recordRegexp is field in schema which uses to define validation type and validation rule for data in cell
+type recordRegexp struct {
 	pattern *regexp.Regexp
 }
 
 // UnmarshalJSON custom unmarshaler
 // This filed can contain list of accepted values and regexp in schema
 // So we need to have custom unmarshaler to define what type is used and set it into struct
-func (c *contains) UnmarshalJSON(data []byte) error {
+func (r *recordRegexp) UnmarshalJSON(data []byte) error {
 	if bytes.EqualFold(data, []byte("null")) {
 		return nil
 	}
 
-	r, err := regexp.Compile(string(bytes.Trim(data, `"`)))
+	exp, err := regexp.Compile(string(bytes.Trim(data, `"`)))
 	if err != nil {
 		return err
 	}
-	c.pattern = r
+	r.pattern = exp
 	return nil
 }
 
 // Contain is checking that the value from cell is meet validation rules from schema
-func (c contains) Contain(value string) bool {
-	return c.pattern.MatchString(value)
+func (r recordRegexp) Contain(value string) bool {
+	return r.pattern.MatchString(value)
 }
 
 // IsNoOp returns true in case this check was empty in schema, and we don't need to process this check
-func (c contains) IsNoOp() bool {
-	return c.pattern.String() == ""
+func (r recordRegexp) IsNoOp() bool {
+	return r.pattern == nil
 }

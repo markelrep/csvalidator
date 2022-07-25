fd6f932daedd95df354fa2b94bd82a1a53345d14
mark <markel3266@gmail.com>
2022-07-25 22:08:57 +0300

    implement contains

---
+++ schema/contain.go
@@ -0,0 +1,29 @@
+package schema
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/go-multierror"
+)
+
+type contains []string
+
+func (c contains) Contain(values map[string]struct{}) (err error) {
+	containMap := make(map[string]struct{})
+
+	for _, contain := range c {
+		containMap[contain] = struct{}{}
+		_, ok := values[contain]
+		if !ok {
+			err = multierror.Append(err, fmt.Errorf("%s is defined in schema, but absent in column", contain))
+		}
+	}
+
+	for v, _ := range values {
+		_, ok := containMap[v]
+		if !ok {
+			err = multierror.Append(err, fmt.Errorf("%s is not defined in schema, but exist in column", v))
+		}
+	}
+	return err
+}
